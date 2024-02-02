f249d565900c083d3c171728ea0e6fd99fe43a21
a <a>
2024-02-02 18:24:21 +0000

    bug fix for current working directory dependency

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ import pytest
 
 @pytest.fixture(scope="session")
 def data_directory() -> pathlib.Path:
-    data_dir = pathlib.Path.cwd() / "data"
+    data_dir = pathlib.Path(__file__).absolute().parent / "data"
     return data_dir
